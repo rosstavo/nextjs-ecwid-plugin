074939d20bca7869472d392f7d1bb9f324598b8a
Developer <developer@mac.lan>
2025-08-01 08:37:22 +0100

    Added additional options to ProductBrowser

--- src/ProductBrowser.js
+++ src/ProductBrowser.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { useEffect } from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
-const ProductBrowser = ({ storeId = 13433173 }) => {
-
+const ProductBrowser = ({
+    storeId,
+    categoriesPerRow = 3,
+    views = "grid(3,3)",
+    list = 10,
+    table = 20,
+    categoryView = "grid",
+    searchView = "list",
+}) => {
     useEffect(() => {
-
         let ecwidLoaded = false;
 
         function load_ecwid() {
-            if (typeof Ecwid != 'undefined') {
+            if (typeof Ecwid != "undefined") {
                 Ecwid.OnAPILoaded.add(function () {
                     if (!ecwidLoaded) {
                         ecwidLoaded = true;
-                        xProductBrowser("categoriesPerRow=3", "views=grid(3,3) list(10) table(20)", "categoryView=grid", "searchView=list", "id=ecStoreProductBrowser");
+                        xProductBrowser(
+                            `categoriesPerRow=${categoriesPerRow}`,
+                            `views=${views} list(${list}) table(${table})`,
+                            `categoryView=${categoryView}`,
+                            `searchView=${searchView}`,
+                            "id=ecStoreProductBrowser"
+                        );
                     }
                 });
             }
+        }
+
+        if (storeId === undefined || storeId === null) {
+            console.error("Store ID is required for ProductBrowser component.");
+            return;
         }
 
         window.ec = window.ec || {};
@@ -28,22 +45,27 @@ const ProductBrowser = ({ storeId = 13433173 }) => {
         window.ecwid_script_defer = true;
         window.ecwid_dynamic_widgets = true;
 
-        if (!document.getElementById('ecwid-script')) {
-            var script = document.createElement('script');
+        if (!document.getElementById("ecwid-script")) {
+            var script = document.createElement("script");
             // script.charset = 'utf-8';
-            script.type = 'text/javascript';
-            script.src = 'https://app.ecwid.com/script.js?' + storeId + '&data_platform=nextjs';
-            script.id = 'ecwid-script'
-            script.onload = load_ecwid
+            script.type = "text/javascript";
+            script.src =
+                "https://app.ecwid.com/script.js?" +
+                storeId +
+                "&data_platform=nextjs";
+            script.id = "ecwid-script";
+            script.onload = load_ecwid;
             document.body.appendChild(script);
         } else {
-            load_ecwid()
+            load_ecwid();
         }
-    })
+    });
 
-    return (
-        <div id="ecStoreProductBrowser"></div>
-    )
-}
+    if (!storeId) {
+        return null;
+    }
 
-export default ProductBrowser
+    return <div id="ecStoreProductBrowser"></div>;
+};
+
+export default ProductBrowser;
